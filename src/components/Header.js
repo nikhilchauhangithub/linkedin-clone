0b6da888f4ef55009fa0dff859968fbdb75420b6
thisisnikhilchauhan <thisisnikhilchauhan@gmail.com>
2023-03-07 00:48:30 +0530

    home section

--- src/components/Header.js
+++ src/components/Header.js
@@ -87,11 +87,14 @@ const Container = styled.div`
   background-color: white;
   border-bottom: 1px solid rgba(0, 0, 0, 0.08);
   left: 0;
-  padding: 5px 24px;
+  padding: 0px 24px;
   position: fixed;
   top: 0;
   width: 100vw;
   z-index: 100;
+  @media (max-width: 768px) {
+    padding: 6px 24px;
+  }
 `;
 
 const Content = styled.div`
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,15 +6,60 @@ import styled from 'styled-components'
 const Home = () => {
   return (
     <Container>
+      
       <Section>
-<h5><a>Hiring in a Hurry? </a></h5>
-<p>LinkedIn is a business and employment-focused social media platform that works through websites and mobile apps</p>
+        
+<h4><a>Are you Hiring, in a Hurry? -</a></h4>
+<p>LinkedIn is a business and employment-focused social media platform that works through websites and mobile apps </p>
       </Section>
     </Container>
   )
 }
 
-const Container =styled.div``;
-const Section =styled.div``;
+const Container =styled.div`
+margin-top: 85px;
+max-width: 100%;
+
+@media (max-width: 768px) {
+  margin-top: 50px;
+      
+    }
+`;
+const Content =styled.div`
+max-width: 1128px;
+margin-left: auto;
+margin-right: auto;
+`;
+const Section =styled.section`
+
+min-height: 50px;
+padding: 16px 0;
+display: flex;
+justify-content: center;
+text-decoration: underline;
+font-size: 16px;
+
+@media (max-width: 768px) {
+  flex-direction: column;
+  text-align: center;
+  letter-spacing: 1.1px;
+   
+    }
+
+h4{
+  letter-spacing: 1.1px;
+  padding-bottom: 4px;
+  color: #0a66c2;
+  
+ 
+  
+}
+
+p{
+  font-size: 16px;
+  color: #434649;
+  font-weight: bold;
+}
+`;
 
 export default Home
