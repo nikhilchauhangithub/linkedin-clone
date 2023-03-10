a00472769b3f7e1a769393003bd3a40b9ca81c87
thisisnikhilchauhan <thisisnikhilchauhan@gmail.com>
2023-03-10 22:53:51 +0530

    main post edit

--- src/components/Main.js
+++ src/components/Main.js
@@ -28,6 +28,68 @@ const Main = () => {
           </button>
         </div>
       </Sharebox>
+      <div>
+        <Article>
+          <SharedActor>
+            <a>
+              <img src="/images/user.svg" alt="" />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+            </a>
+            <button>
+              <img src="/images/ellipsis.svg" alt="" />
+            </button>
+          </SharedActor>
+          <Description>Description</Description>
+          <SharedImg>
+            <a>
+              <img src="\images\shared-image.jpg" alt="" />
+            </a>
+          </SharedImg>
+          <SocialCounts>
+            <li>
+              <button>
+                <img
+                  src="https://www.svgrepo.com/show/499778/like.svg"
+                  alt=""
+                />
+                <img
+                  src="https://www.svgrepo.com/show/404975/clapping-hands.svg"
+                  alt=""
+                />
+                <span>80</span>
+              </button>
+            </li>
+            <li>
+              <a>8 comments</a>
+            </li>
+          </SocialCounts>
+          <SocialActions>
+            <button>
+              <img src="https://www.svgrepo.com/show/499778/like.svg" alt="" />
+              <span>Like</span>
+            </button>
+            <button>
+              <img src="https://www.svgrepo.com/show/499759/edit.svg" alt="" />
+              <span>Comments</span>
+            </button>
+            <button>
+              <img src="https://www.svgrepo.com/show/499806/share.svg" alt="" />
+              <span>Share</span>
+            </button>
+            <button>
+              <img
+                src="https://www.svgrepo.com/show/343522/telegram-communication-chat-interaction-network-connection.svg"
+                alt=""
+              />
+              <span>Send</span>
+            </button>
+          </SocialActions>
+        </Article>
+      </div>
     </Container>
   );
 };
@@ -44,7 +106,6 @@ const Commoncard = styled.div`
   border-radius: 8px;
   position: relative;
   border: none;
-  wid
 `;
 
 const Sharebox = styled(Commoncard)`
@@ -108,6 +169,135 @@ const Sharebox = styled(Commoncard)`
           background-color: rgba(0, 0, 0, 0.15);
           color: #0a66c2;
         }
+      }
+    }
+  }
+`;
+
+const Article = styled(Commoncard)`
+  padding: 0;
+  margin: 0 0 8px;
+  overflow: visible;
+`;
+
+const SharedActor = styled.div`
+  padding: 15px;
+
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+
+    img {
+      width: 48px;
+      border-radius: 50%;
+    }
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 16px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+  button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+    img {
+      padding-top: 10px;
+      height: 17px;
+    }
+  }
+`;
+
+const Description = styled.div``;
+
+const SharedImg = styled.div`
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-color: #f9fafb;
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+`;
+const SocialCounts = styled.ul`
+  img {
+    height: 25px;
+    padding-right: 4px;
+  }
+  line-height: 1.5;
+  display: flex;
+  /* align-items: flex-start; */
+  /* overflow: auto; */
+  /* background: transparent; */
+  margin: 0 16px;
+  padding: 8px 0;
+  border-bottom: 1px solid #e9e5df;
+  list-style: none;
+  li {
+    margin-right: 5px;
+    font-size: 15px;
+    margin-top: auto;
+    margin-bottom: auto;
+    button {
+      display: flex;
+      align-items: center;
+      border: none;
+      background: rgba(0, 0, 0, 0.15);
+      border-radius: 8px;
+      padding: 6px;
+    }
+  }
+`;
+
+const SocialActions = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-around;
+  min-height: 40px;
+  button {
+    display: inline-flex;
+    align-items: center;
+    border-radius: 8px;
+    border: none;
+    background: transparent;
+    padding: 5px;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.15);
+    }
+    img {
+      height: 20px;
+      width: 30px;
+    }
+
+    @media (max-width: 768px) {
+      span {
+        margin-left: 8px;
       }
     }
   }
