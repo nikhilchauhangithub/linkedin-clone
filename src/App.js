2e74a7a77d83cee59d2b66ee475c61f9b43438f8
thisisnikhilchauhan <thisisnikhilchauhan@gmail.com>
2023-03-11 23:13:42 +0530

    redux initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Header from './components/Header';
-import Home from './components/Home';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>} />
-        
-        <Route path='/home' element={<><Header/><Home/></>}>
-          </Route>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
 
-       
-      </Routes>
-      </BrowserRouter>
-    </div>
+            <Route
+              path="/home"
+              element={
+                <>
+                  <Header />
+                  <Home />
+                </>
+              }
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
